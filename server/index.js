2b962aece3762a8f7df1450076c9ad9533e63fc0
Muhammad Arslan <dev_arslan@unblinked.com>
2021-12-21 16:26:52 +0500

    2 apis added and deployed

--- server/index.js
+++ server/index.js
@@ -3,11 +3,11 @@ const app = express()
 
 const PORT = 5000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send("Gist Swap Api will be listed here on NODE server")
 })
 
-app.get('/user', (req, res) => {
+app.get('api/user', (req, res) => {
     res.send("I'm dummy User from Node Server")
 })
 
