38cbea698a275b6dab4e2aa6a80639ccee9ecff2
Muhammad Arslan <dev_arslan@unblinked.com>
2021-12-21 13:47:57 +0500

    html file added

--- src/Controllers/User/index.ts
+++ src/Controllers/User/index.ts
@@ -1,6 +1,5 @@
 
 import { Request, Response  } from "express"
-
 export const getUser = function (req: Request, res: Response) {
-    res.sendFile(process.cwd() + '/index.html');
+    res.sendFile(process.cwd() + '../../../index.html');
 }
