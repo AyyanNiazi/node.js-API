84bbde43687989c89df4f8d720af981a71247d9b
Muhammad Arslan <dev_arslan@unblinked.com>
2021-12-21 16:27:40 +0500

    2 apis added and deployed

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.get('/api', (req, res) => {
     res.send("Gist Swap Api will be listed here on NODE server")
 })
 
-app.get('api/user', (req, res) => {
+app.get('/api/user', (req, res) => {
     res.send("I'm dummy User from Node Server")
 })
 
