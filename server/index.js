b96739ab83951e17ffe090683ce070d06e1533d9
Muhammad Arslan <dev_arslan@unblinked.com>
2021-12-21 15:50:37 +0500

    changes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send("Hello worlds")
 })
 
+app.get('/user', (req, res) => {
+    res.send("I'm dummy User")
+})
+
 app.listen(process.env.PORT || PORT, () => {
     console.log('server is running on 5000')
 })
