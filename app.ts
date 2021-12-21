5b1c21571e2df8f69799714b4391cacc50f4a5dd
Muhammad Arslan <dev_arslan@unblinked.com>
2021-12-21 14:21:06 +0500

    app.ts chnage to outer folder

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 import * as dotenv from "dotenv";
 import express from "express";
-import cors from "cors";
 import helmet from "helmet";
 
 // Routes
@@ -17,7 +16,6 @@ if (!process.env.PORT) {
  const app = express();
 
  app.use(helmet());
- app.use(cors());
  app.use(express.json());
 
  app.get('/', (req, res) => {
